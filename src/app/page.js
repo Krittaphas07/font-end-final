c4c7dfb0e6dd243f76ceae96c19d8f3e934b6ccd
tennousei.dandan <arnakorn567@gmail.com>
2024-08-05 14:30:52 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+import'bootstrap/dist/css/bootstrap.min.css';
+import Link from "next/link";
+
 export default function Home() {
   return (
 <>
@@ -10,7 +13,7 @@ export default function Home() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/about">About</a>
@@ -30,7 +33,7 @@ export default function Home() {
       </ul>
       <form className="d-flex" role="Sign">
         <button className="btn btn-outline-success" type="submit">sign in</button>
-        <button className="btn btn-outline-success" type="submit">sign up</button>
+        <Link href="signup" className="btn btn-outline-success" type="submit">sign up</Link>
       </form>
     </div>
     </div>
---
+++ src/app/signup/page.js
@@ -0,0 +1,93 @@
+import Link from "next/link";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Page() {
+    return (
+  <>
+  <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Sodaft</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">About</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="/ser" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Service
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">contact</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="Sign">
+        <button className="btn btn-outline-success" type="submit">sign in</button>
+        <Link href="signup" className="btn btn-outline-success" type="submit">sign up</Link>
+      </form>
+    </div>
+    </div>
+</nav>
+
+  <br /><br /><br />
+  <div classname="container">
+    <form className="row g-3">
+      <div className="col-md-6">
+        <label htmlFor="inputEmail4" className="form-label">Email</label>
+        <input type="email" className="form-control" id="inputEmail4" />
+      </div>
+      <div className="col-md-6">
+        <label htmlFor="inputPassword4" className="form-label">Password</label>
+        <input type="password" className="form-control" id="inputPassword4" />
+      </div>
+      <div className="col-12">
+        <label htmlFor="inputAddress" className="form-label">Address</label>
+        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+      </div>
+      <div className="col-12">
+        <label htmlFor="inputAddress2" className="form-label">Address 2</label>
+        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+      </div>
+      <div className="col-md-6">
+        <label htmlFor="inputCity" className="form-label">City</label>
+        <input type="text" className="form-control" id="inputCity" />
+      </div>
+      <div className="col-md-4">
+        <label htmlFor="inputState" className="form-label">State</label>
+        <select id="inputState" className="form-select">
+          <option selected>Choose...</option>
+          <option>...</option>
+        </select>
+      </div>
+      <div className="col-md-2">
+        <label htmlFor="inputZip" className="form-label">Zip</label>
+        <input type="text" className="form-control" id="inputZip" />
+      </div>
+      <div className="col-12">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" id="gridCheck" />
+          <label className="form-check-label" htmlFor="gridCheck">
+            Check me out
+          </label>
+        </div>
+      </div>
+      <div className="col-12">
+        <button type="submit" className="btn btn-primary">Sign in</button>
+      </div>
+    </form>
+  </div>
+  </>
+  
+  );
+  }
+  
